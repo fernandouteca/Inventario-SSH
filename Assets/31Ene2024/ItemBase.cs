6965fdd43ca577615560f18fe84b16d67ad57438
fernandouteca <diseno.uteca@gmail.com>
2024-02-02 15:00:03 -0600

    Initial commit

---
+++ Assets/31Ene2024/ItemBase.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBase : MonoBehaviour
+{
+    string nombre;
+    //sprite
+    int vida;
+    string descripcion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Inventario-SSH
+ Aquí se encontrará todo para el proyecto
